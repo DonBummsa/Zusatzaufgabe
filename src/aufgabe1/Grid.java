7527cca88c59188b4e0adc125f0eef83dd5ab140
kekde <kekde@193.196.96.102>
2022-04-08 10:34:07 +0200

    08.04 achsen + beschriftung

--- src/aufgabe1/Grid.java
+++ src/aufgabe1/Grid.java
@@ -1,79 +1,114 @@
 package aufgabe1;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
 import java.applet.*;
+
 public class Grid extends JFrame {
-    public static int width = 1024;
-    public static int height = 786;
+	public static int width = 1024;
+	public static int height = 786;
 
-    /**
-     * Konstruktor aus der Übung
-     */
-    public Grid() {
-        super();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(width, height);
-        this.setTitle("Plotter");
-        this.setVisible(true);
-    }
+	/**
+	 * Konstruktor aus der Übung
+	 */
+	public Grid() {
+		super();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(width, height);
+		this.setTitle("Plotter");
+		this.setVisible(true);
+	}
 
-    public static void main(String[] args) {
-        new Grid();
-    }
-    int center_x = 1024;
-    int center_y = 512;
-    // TODO: implementiere ein Koordinatenkreuz
-    /**
-     * coordinates zeichnet ein Koordinatenkreuz.
-     */
-    public void coordinates(Graphics g) {
-    	Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(3));
-        g2.draw(new Line2D.Float(512, 786, 512, 0));	// achse 1 
-        g2.draw(new Line2D.Float(0,383,1023,383)); // achse 2
-        g2.setStroke(new BasicStroke(1));
-    	for(int i = 0;i<1024; i++ ) {
-    		
-    		g.setColor(Color.LIGHT_GRAY);
-    		if(i==512) {
-    			i++;
-    		}
-    		if(i % 8 == 0) {
-    			g.drawLine(8*i , 0 , 8*i , 1024 );
-    			g.drawLine(0, 8*i , 1024 , 8*i );
-    			
-    		}
-    		
-    		
-    	}
-    	
-    }
+	public static void main(String[] args) {
+		new Grid();
+	}
 
-    // TODO: implementiere eine Achsenbeschriftung
-    /**
-     * labels zeichnet Achsenbeschriftungen bei ganzen Zahlen.
-     */
-    public void labels(Graphics g) {
+	int center_x = 1024;
+	int center_y = 512;
 
-    }
+	// TODO: implementiere ein Koordinatenkreuz
+	/**
+	 * coordinates zeichnet ein Koordinatenkreuz.
+	 */
+	public void coordinates(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.translate(width / 2, height / 2);
+		g2.setStroke(new BasicStroke(1));
+		Color lighterGray = new Color(211, 211, 211); // Color white
+		for (int i = -width / 2; i < width / 2; i++) {
 
-    // TODO: implementiere einen Funktionsplot.
-    /**
-     * plot zeichnet die durch die Expression e angegebene Funktion in der angegebenen Farbe in g.
-     */
-    public void plot(Graphics g, Expression e, Color c) {
+			if (i % 16 == 0) {
 
-    }
+				g2.setColor(lighterGray);
+				g2.drawLine(i, -width, i, width);
+				g2.drawLine(-height, i, height, i);
 
-    /**
-     * paint-Methode wie in der Vorlesung
-     */
-    public void paint(Graphics g) {
-        coordinates(g);
-        labels(g);
-        // TODO: Hier weitere Beispiele einfügen
-        // TODO: An diese Stelle sollst Du auch in den spÃ¤teren Aufgabenteilen jeweils Funktionen in unterschiedlicher Syntax schreiben.
-        plot(g, new HardcodedSin(), Color.RED);
-    }
+			}
+
+			g2.setColor(Color.LIGHT_GRAY);
+			if (i % 80 == 0) {
+				g2.drawLine(i, -width, i, width);
+				g2.drawLine(-height, i, height, i);
+
+			}
+
+		}
+		g2.setStroke(new BasicStroke(3));
+		g2.setColor(Color.BLACK);
+		g2.draw(new Line2D.Float(width / 2, 0, -width / 2, 0)); // achse 1
+		g2.draw(new Line2D.Float(0, height / 2, 0, -height / 2)); // achse 2
+	}
+
+	// TODO: implementiere eine Achsenbeschriftung
+	/**
+	 * labels zeichnet Achsenbeschriftungen bei ganzen Zahlen.
+	 */
+	public void labels(Graphics g) {
+
+		Graphics2D g2d = (Graphics2D) g;
+		for (int i = -width / 2; i < width / 2; i++) {
+
+			String a = String.valueOf(-i / 80);
+			if (i % 80 == 0) {
+				g2d.drawString(a, i, -2);
+			}
+			if (i != 0 && i % 80 == 0) {
+				g2d.drawString(a, 2, i);
+
+			}
+
+		}
+
+	}
+
+	// TODO: implementiere einen Funktionsplot.
+	/**
+	 * plot zeichnet die durch die Expression e angegebene Funktion in der
+	 * angegebenen Farbe in g.
+	 */
+	public void plot(Graphics g, Expression e, Color c) {
+
+		Graphics2D g1 = (Graphics2D) g;
+		
+		for(int i = -7; i<8; i++) {
+		
+		
+		g1.setStroke(new BasicStroke(2));
+		g1.setColor(c);
+		
+		}
+	}
+
+	/**
+	 * paint-Methode wie in der Vorlesung
+	 */
+	public void paint(Graphics g) {
+		coordinates(g);
+		labels(g);
+		// TODO: Hier weitere Beispiele einfügen
+		// TODO: An diese Stelle sollst Du auch in den spÃ¤teren Aufgabenteilen jeweils
+		// Funktionen in unterschiedlicher Syntax schreiben.
+		plot(g, new HardcodedSin(), Color.RED);
+	}
 }
