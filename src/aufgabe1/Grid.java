1cb57393c1ea498242d3f519b6ad518d715ade00
kekde <kekde@193.196.96.102>
2022-04-12 10:22:13 +0200

    fertig

--- src/aufgabe1/Grid.java
+++ src/aufgabe1/Grid.java
@@ -1,16 +1,17 @@
 package aufgabe1;
- 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.applet.*;
- 
+
 public class Grid extends JFrame {
 	public static int width = 1024;
 	public static int height = 786;
 	public static int largetick = 80; // entspricht 1
-	public static int smalltick = 16; // entspricht 0,2 
+	public static int smalltick = 16; // entspricht 0,2
+
 	/**
 	 * Konstruktor aus der ‹bung
 	 */
@@ -21,118 +22,116 @@ public class Grid extends JFrame {
 		this.setTitle("Plotter");
 		this.setVisible(true);
 	}
- 
+
 	public static void main(String[] args) {
 		new Grid();
 	}
-	
-	
- 
+
 	// TODO: implementiere ein Koordinatenkreuz
 	/**
 	 * coordinates zeichnet ein Koordinatenkreuz.
 	 */
 	public void coordinates(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.WHITE); 			// weiﬂer hintergrund
-		g2.fillRect(0,0,width,height); 
-		g2.translate(width / 2, height / 2);	// koordinaten auf den mittelpunkt verschieben
-		
+		g2.setColor(Color.WHITE); // weiﬂer hintergrund
+		g2.fillRect(0, 0, width, height);
+		g2.translate(width / 2, height / 2); // koordinaten auf den mittelpunkt verschieben
+
 		g2.setStroke(new BasicStroke(1));
 		Color lighterGray = new Color(211, 211, 211); // Color white.grey
 		for (int i = -width / 2; i < width / 2; i++) {
- 
+
 			if (i % smalltick == 0) {
- 
+
 				g2.setColor(lighterGray);
 				g2.drawLine(i, -width, i, width);
 				g2.drawLine(-height, i, height, i);
- 
+
 			}
- 
+
 			g2.setColor(Color.LIGHT_GRAY);
 			if (i % largetick == 0) {
 				g2.drawLine(i, -width, i, width);
 				g2.drawLine(-height, i, height, i);
- 
+
 			}
- 
+
 		}
 		g2.setStroke(new BasicStroke(3));
 		g2.setColor(Color.BLACK);
 		g2.draw(new Line2D.Float(width / 2, 0, -width / 2, 0)); // achse 1
 		g2.draw(new Line2D.Float(0, height / 2, 0, -height / 2)); // achse 2
 	}
- 
+
 	// TODO: implementiere eine Achsenbeschriftung
 	/**
 	 * labels zeichnet Achsenbeschriftungen bei ganzen Zahlen.
 	 */
 	public void labels(Graphics g) {
-		
+
 		Graphics2D g2d = (Graphics2D) g;
-		
+
 		g2d.setColor(Color.BLACK);
 		for (int i = -width / 2; i < width / 2; i++) {
-			
-						
+
 			String a = String.valueOf(-i / largetick);
-			
+
 			FontMetrics babada = g2d.getFontMetrics();
 			Rectangle2D rec2d = babada.getStringBounds(a, g2d);
-			
-			if (i % largetick == 0 && i != 0 ) {
-			
+
+			if (i % largetick == 0 && i != 0) {
+
 				g2d.setColor(Color.WHITE);
-				g2d.fillRect(-i, -6-(int)rec2d.getHeight(),(int)rec2d.getWidth(),(int)rec2d.getHeight());
+				g2d.fillRect(-i, -6 - (int) rec2d.getHeight(), (int) rec2d.getWidth(), (int) rec2d.getHeight());
 				g2d.setColor(Color.BLACK);
 				g2d.drawString(a, -i, -6);
 			}
-			if ( i != 0 && i % largetick == 0) {
+			if (i != 0 && i % largetick == 0) {
 				g2d.setColor(Color.WHITE);
-				g2d.fillRect(6, i-(int)rec2d.getHeight(),(int)rec2d.getWidth(),(int)rec2d.getHeight());
+				g2d.fillRect(6, i - (int) rec2d.getHeight(), (int) rec2d.getWidth(), (int) rec2d.getHeight());
 				g2d.setColor(Color.BLACK);
 				g2d.drawString(a, 6, i);
-				
+
 			}
- 
+
 		}
- 
+
 	}
- 
+
 	// TODO: implementiere einen Funktionsplot.
 	/**
 	 * plot zeichnet die durch die Expression e angegebene Funktion in der
 	 * angegebenen Farbe in g.
 	 */
 	public void plot(Graphics g, Expression e, Color c) {
- 
+
 		Graphics2D g1 = (Graphics2D) g;
 		int[] xpoints = new int[width];
 		int[] ypoints = new int[width];
+
 		for (int i = -width / 2; i < width / 2; i++) {
-			int wert_1 = (int) Math.round(e.eval((double)i/largetick)*largetick ); //x werte der funktion in f
+			int wert_1 = (int) Math.round(e.eval((double) i / largetick) * largetick); // x werte der funktion in f
+//			if(wert_1==0)
+//				continue;		// f¸r sqrt?
 			ypoints[i + width / 2] = -wert_1;
 			xpoints[i + width / 2] = i;
 
 		}
 		g1.setColor(c);
-	
-		for (int n = 0; n < width-1 ; n++) {
-			g1.drawLine(xpoints[n], ypoints[n], xpoints[n + 1], ypoints[n + 1]);	// der malt linien
 
-			
+		for (int n = 0; n < width - 1; n++) {
+
+//			System.out.println(ypoints[n]);	//debug
+			g1.drawLine(xpoints[n], ypoints[n], xpoints[n + 1], ypoints[n + 1]); // der malt linien
+
 		}
-		
+
 	}
-	
- 
+
 	/**
 	 * paint-Methode wie in der Vorlesung
 	 */
 	public void paint(Graphics g) {
-		
-		
 		coordinates(g);
 		plot(g, new HardcodedSin(), Color.RED);
 		plot(g, new HardcodedTan(), Color.GREEN);
@@ -142,7 +141,6 @@ public class Grid extends JFrame {
 		// TODO: Hier weitere Beispiele einf¸gen
 		// TODO: An diese Stelle sollst Du auch in den sp√§teren Aufgabenteilen jeweils
 		// Funktionen in unterschiedlicher Syntax schreiben.
-		
+
 	}
 }
- 
